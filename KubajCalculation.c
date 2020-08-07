abfc41765b7484046b412e392374a56ec2c57506
Erdem <57351007+ErdemCmd@users.noreply.github.com>
2020-08-07 15:10:10 +0300

    Update KubajCalculation.c

--- KubajCalculation.c
+++ KubajCalculation.c
@@ -2,35 +2,30 @@
 #include <math.h>
 #include <locale.h>
 
-#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078164
-
 int main()
 {
 	setlocale(LC_ALL, "Turkish");
 
-	/* Yazılım Gereksinimleri;
+		/// Bilinenleri isteyelim.
+	// Alan değerleri; zayıf zemin kazı alanı (Azz1…Azz101), yarma alan (Ay1…Ay101), dolgu alan (Ad1….Ad101)
 
-	1- Bilinenlerin tanımlanması
+	// Mesafeler; kesitler arası mesafeler (S1….S101) 
 
-	1.1- Alan değerleri; Zayıf Zemin Kazı Alanı(Azz1…Azz101), Yarma Alan(Ay1…Ay101), Dolgu Alan(Ad1….Ad101)
 
-	1.2- Mesafeler; Kesitler arası mesafeler (S1….S101)
+		/// Bilinmeyenlerin hesaplanması.
+	printf("\n\t=== Hesap sonuçları! === \n");
+	/* Hacim hesabı; zayıf zemin hacim (Vzz1..Vzz101), yarma hacim (Vy1…Vy101), dolgu hacim (Vd1…Vd101)
+	                    * Vzz(n) = (Azz(n) + Azz(n+1)) / 2
+	                    * Vy(n) = (Ay(n) + Ay(n+1)) / 2
+	                    * Vd(n) = (Ad(n) + Ad(n+1)) / 2 */
 
-	2- Bilinmeyenlerin Hesaplanması
+	/* Kümülatif hacim hesabı; zayıf zemin kümülatif hacim (KVzz1..KVzz101), yarma kümülatif hacim (KVy1…KVy101), dolgu kümülatif hacim (KVd1…KVd101)
+	                    * KVzz(n) = (Kvzz(n - 1) + Vzz(n))
+	                    * KVy(n) = (Kvy(n - 1) + Vy(n))
+	                    * KVd(n) = (Kvd(n - 1) + Vd(n)) */
 
-	2.1- Hacim Hesabı; Zayıf Zemin Hacim(Vzz1..Vzz101), Yarma Hacim(Vy1…Vy101), Dolgu Hacim(Vd1…Vd101)
-
-	                     * Vzz(n)=(Azz(n)+Azz(n+1))/2
-	                     * Vy(n)=(Ay(n)+Ay(n+1))/2
-	                     * Vd(n)=(Ad(n)+Ad(n+1))/2
-	2.2- Kümülatif Hacim Hesabı; Zayıf Zemin Kümülatif Hacim(KVzz1..KVzz101), Yarma Kümülatif Hacim(KVy1…KVy101), Dolgu Kümülatif Hacim(KVd1…KVd101)
-
-	                      * KVzz(n)=(Kvzz(n-1)+Vzz(n))
-	                      * KVy(n)=(Kvy(n-1)+Vy(n))
-	                      * KVd(n)=(Kvd(n-1)+Vd(n))
-	2.3- Brükner Değeri Hesabı; Brükner değeri (brk1…brk101)
-
-	                      * brk(n)=(KVy(n)-KVd(n)) */
+	/* Brükner değeri hesabı; brükner değeri (brk1…brk101)
+	                    * brk(n) = (KVy(n) - KVd(n)) */
 
 	return 0;
 }
